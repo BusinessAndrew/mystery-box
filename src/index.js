0bff0d82ac13b5387c90ae133be877d50c1a5f23
BusinessAndrew <andrew.business.work@gmail.com>
2023-10-14 16:49:48 +0100

    delete service worker

--- src/index.js
+++ src/index.js
@@ -24,23 +24,6 @@ night === "true"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// In your index.js or app.js
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
-      });
-  });
-}
-
 root.render(
   <Provider store={store}>
     <CookiesProvider>
